9ebbe351bb5f8214b56b1d1747e0a9114bfa4890
linalu1 <linalu001@gmail.com>
2015-07-24 15:27:09 -0700

    Progress on first 2 BRs

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  message VARCHAR(160)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  /* Describe your table here.*/
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  roomName VARCHAR(15)
+);
 
 
 
